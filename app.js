9b3a569a32d600d0c82a638ea84cf4ffc22e59c8
Jaycee Porto <jaycee.porto@outlook.com>
2022-07-14 19:53:59 -0500

    Completed Exercise

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ console.log("EXERCISE 1:\n==========\n");
 const http = require("http");
 
 // Finish setting up the server
+http
+.createServer (function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+})
+.listen(3000, function () {
+    console.log("Server listening...")}
+)
